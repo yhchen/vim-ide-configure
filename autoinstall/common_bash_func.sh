699926cf80e0650d3e9de9b4ef8559bdfe79bf6c
yhchen <myself559@gmail>
2015-07-12 09:54:45 -0400

    add auto run shell

--- autoinstall/common_bash_func.sh
+++ autoinstall/common_bash_func.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 # check ${1} command exist.
 # return 0 if exists. otherwise return none zero.
 function command_exist()
@@ -12,20 +13,24 @@ function command_exist()
 	fi
 }
 
+
 # init ${SUDO} for super user permission
 # warming : this command require you enter password
 function require_sudo()
 {
-	echo "please enter your password:"
-	read _passwd_
-	SUDO="eval echo ${_passwd_} | sudo -S "
+	if [[ -z "${SUDO}" || ${1} -eq 1 ]]; then
+		echo "please enter your password:"
+		read _passwd_
+		SUDO="eval echo ${_passwd_} | sudo -S "
+	fi;
 }
+
 
 # get install command
 function get_install_cmd()
 {
 	if [ `command_exist yum` == 0 ]; then
-		echo yum;
+		echo yum -y ;
 	elif [ `command_exist apt-get` == 0 ]; then
 		echo apt-get;
 	else
@@ -33,4 +38,20 @@ function get_install_cmd()
 	fi
 }
 
+# clean install temporary files
+function autoclean_install()
+{
+	if [[ `command_exist yum` = 0 ]]; then
+		echo "clean up system";
+		${SUDO} yum clean all;
+	elif [[ `command_exist apt-get` = 0 ]]; then
+		echo "clean up system";
+		${SUDO} apt-get autoclean;
+		${SUDO} apt-get clean;
+		${SUDO} apt-get autoremove;
+		${SUDO} apt-get remove tracker;
+		${SUDO} rm -rf /var/cache/apt/archives;
+		${SUDO} rm -rf /var/cache/apt/archives/partial;
+	fi
+}
 
--- autoinstall/install_bash_env.sh
+++ autoinstall/install_bash_env.sh
@@ -4,12 +4,21 @@ source "./common_bash_func.sh"
 require_sudo
 
 _pwd_=`pwd`
-_install_vim_="vim vim-common vim-filesystem "
-_install_dev_="libreadline-dev gcc gcc-g++ ctags "
-_install_all_="${_install_vim_} ${_install_dev_}"
+_install_vim_="vim vim-common vim-filesystem vim-gnome "
+#_install_mysql_="mysql-client libmysqlclient-dev mysql-server "
+_install_sys_tools_="htop zerofree subversion uuid-dev "
+_install_dev_="libreadline-dev gcc gcc-g++ ctags gdb cgdb build-essential libxml2-dev "
+_install_input_="ibus ibus-pinyin "
+#_install_dev2_="linux-headers-$(uname -r) gtkorphan gnome-system-tools"
+_install_all_="${_install_vim_} ${_install_dev_} ${_install_input_} ${_install_sys_tools_} ${_install_mysql_} ${_install_dev2_} "
 
 
 _install_cmd_=`get_install_cmd`
 ${SUDO} ${_install_cmd_} install ${_install_all_}
+echo "fix libxml2 header";
+${SUDO} cp -r /usr/include/libxml2/libxml /usr/include/;
+
+# auto clean install cache
+autoclean_install
 
 
